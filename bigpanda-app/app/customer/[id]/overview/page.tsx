7f615aa22f5e667eea005d933c3a8651d3b4b1ea
Josh Miloslavsky <jmiloslavsky@jmiloslavsky-G3M4YF006K.local>
2026-04-03 08:05:58 -0700

    feat(35-05): wire WeeklyFocus into Overview page as first component
    - Added WeeklyFocus import
    - Positioned WeeklyFocus above OnboardingDashboard per plan spec
    - TypeScript compilation clean for overview page

--- bigpanda-app/app/customer/[id]/overview/page.tsx
+++ bigpanda-app/app/customer/[id]/overview/page.tsx
@@ -1,3 +1,4 @@
+import { WeeklyFocus } from '../../../../components/WeeklyFocus'
 import { OnboardingDashboard } from '../../../../components/OnboardingDashboard'
 import { OverviewMetrics } from '../../../../components/OverviewMetrics'
 import { HealthDashboard } from '../../../../components/HealthDashboard'
@@ -12,6 +13,7 @@ export default async function OverviewPage({
 
   return (
     <div className="space-y-6 py-4">
+      <WeeklyFocus projectId={projectId} />
       <OnboardingDashboard projectId={projectId} />
       <OverviewMetrics projectId={projectId} />
       <HealthDashboard projectId={projectId} />
